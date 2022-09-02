c9906adcb1101afd827d9bf3d67b5ec11b35cd94
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-09-02 16:27:21 +0600

    initial data load from api show console

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
 // https://fakestoreapi.com/products
-console.log('Helloo js');
+// console.log('Helloo js');
+
+const loadProducts = async () => {
+
+    const response = await fetch('https://fakestoreapi.com/products');
+    console.log(response);
+}
+
+loadProducts();
