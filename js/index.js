3aba2683bb6631775d602aa951be6f91405d72c2
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-09-02 16:32:08 +0600

    show console api json data in array

--- js/index.js
+++ js/index.js
@@ -4,7 +4,8 @@
 const loadProducts = async () => {
 
     const response = await fetch('https://fakestoreapi.com/products');
-    console.log(response);
+    const data = await response.json();
+    console.log(data);
 }
 
 loadProducts();
