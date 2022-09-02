8ffec409c91e92ff28aad86db9ee21068fc9cb41
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-09-02 16:45:42 +0600

    show promise in console

--- js/index.js
+++ js/index.js
@@ -2,10 +2,18 @@
 // console.log('Helloo js');
 
 const loadProducts = async () => {
-
     const response = await fetch('https://fakestoreapi.com/products');
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
+    return data; // ekhon amra amdr proyjon data gula bar abr use korte parbo
 }
+//create func-01
+// two types of promise 1. resolved 2. reject
+const setAllMenue = () => {
+    console.log(loadProducts());
+}
+setAllMenue();
 
-loadProducts();
+
+
+// loadProducts();
